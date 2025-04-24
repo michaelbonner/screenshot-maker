2ad67e22aadd1c4fbab2b4a2b37ad9ad18ef9587
Michael Bonner <michaelwbonner@gmail.com>
2025-04-24 12:32:18 -0600

    quick docs on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,98 @@
+import Link from "next/link";
+
+const args = [
+  {
+    name: "url",
+    type: "string",
+    description: "The URL of the page to screenshot",
+    required: true,
+  },
+  {
+    name: "width",
+    type: "number?",
+    description: "The width of the screenshot",
+  },
+  {
+    name: "height",
+    type: "number?",
+    description: "The height of the screenshot",
+  },
+  {
+    name: "scale",
+    type: "number?",
+    description:
+      "The scale of the screenshot. Helpful to get a desktop screenshot, but scaled down for rendering.",
+  },
+  {
+    name: "quality",
+    type: "number?",
+    description: "The quality of the screenshot",
+  },
+];
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="grid gap-24 min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="max-w-7xl mx-auto grid gap-12">
         <h1 className="text-4xl font-bold">Screenshot Maker</h1>
+        <div className="grid gap-8 [&_h2]:text-2xl [&_h2]:font-bold [&_h2]:mb-4">
+          <div>
+            <h2>Example</h2>
+            <div>
+              <code className="block text-sm border rounded-md p-4">
+                <Link
+                  className="hover:underline underline-offset-4"
+                  href="http://localhost:3000/api/screenshot?url=https%3A%2F%2Fbootpackdigital.com&width=1920&height=1080&scale=0.25&quality=50"
+                  target="_blank"
+                >
+                  http://localhost:3000/api/screenshot?url=https%3A%2F%2Fbootpackdigital.com&width=1920
+                  <wbr />
+                  &height=1080
+                  <wbr />
+                  &scale=0.25
+                  <wbr />
+                  &quality=50
+                </Link>
+              </code>
+            </div>
+          </div>
+          <div>
+            <h2>Args</h2>
+            <table className="w-full border-collapse border border-gray-300 [&_td]:p-2 [&_th]:p-2 [&_th]:border [&_td]:border [&_th]:border-gray-300">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Type</th>
+                  <th>Description</th>
+                </tr>
+              </thead>
+              <tbody>
+                {args.map((arg) => (
+                  <tr key={arg.name}>
+                    <td>{arg.name}</td>
+                    <td>
+                      <code>{arg.type}</code>
+                    </td>
+                    <td>{arg.description}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </main>
+      <footer className="max-w-7xl mx-auto">
+        <p className="text-sm">
+          ©2023-{new Date().getFullYear()}{" "}
+          <a
+            className="underline underline-offset-4"
+            href="https://bootpackdigital.com"
+          >
+            Bootpack Digital
+          </a>
+          . All rights reserved.
+        </p>
+      </footer>
     </div>
   );
 }
