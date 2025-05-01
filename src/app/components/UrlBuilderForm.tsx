43505607e98a18adf069a80ad1211847e15774dc
Michael Bonner <mike@bootpackdigital.com>
2025-05-01 10:16:38 -0600

    DRY up the form a bit

--- src/app/components/UrlBuilderForm.tsx
+++ src/app/components/UrlBuilderForm.tsx
@@ -1,7 +1,14 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
+import {
+  FormProvider,
+  SubmitHandler,
+  useForm,
+  useFormContext,
+} from "react-hook-form";
 import { z } from "zod";
 import {
   DEFAULT_HEIGHT,
@@ -10,8 +17,6 @@ import {
   DEFAULT_WIDTH,
   inputSchema,
 } from "../api/screenshot/validation";
-import { useEffect, useState } from "react";
-import { zodResolver } from "@hookform/resolvers/zod";
 
 const url = "https://bootpackdigital.com";
 
@@ -35,11 +40,7 @@ export const UrlBuilderForm = () => {
     );
   }, []);
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<Inputs>({
+  const methods = useForm<Inputs>({
     defaultValues: {
       url,
     },
@@ -70,137 +71,55 @@ export const UrlBuilderForm = () => {
 
   return (
     <div className="grid gap-8 border p-8">
-      <form
-        method="GET"
-        action="/api/screenshot"
-        target="_blank"
-        className="grid gap-x-4 gap-y-6"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <div>
-          <label className={styles.label} htmlFor="url">
-            URL
-          </label>
-          <input
-            className={styles.inputText}
-            type="text"
+      <FormProvider {...methods}>
+        <form
+          method="GET"
+          action="/api/screenshot"
+          target="_blank"
+          className="grid gap-x-4 gap-y-6"
+          onSubmit={methods.handleSubmit(onSubmit)}
+        >
+          <InputField
+            label="URL"
             id="url"
+            type="text"
             placeholder="https://..."
-            {...register("url")}
           />
-          {errors.url && (
-            <span className={styles.error}>{errors.url.message}</span>
-          )}
-        </div>
-        <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4">
-          <div>
-            <label className={styles.label} htmlFor="width">
-              Width
-            </label>
-            <input
-              className={styles.inputText}
+          <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4">
+            <InputField
+              label="Width"
               id="width"
-              max={3840}
-              min={1}
+              type="number"
               placeholder={DEFAULT_WIDTH.toString()}
-              step={1}
-              type="number"
-              {...register("width")}
             />
-            {errors.width && (
-              <span className={styles.error}>{errors.width.message}</span>
-            )}
-          </div>
-          <div>
-            <label className={styles.label} htmlFor="height">
-              Height
-            </label>
-            <input
-              className={styles.inputText}
+            <InputField
+              label="Height"
               id="height"
-              max={2160}
-              min={1}
+              type="number"
               placeholder={DEFAULT_HEIGHT.toString()}
-              step={1}
-              type="number"
-              {...register("height")}
             />
-            {errors.height && (
-              <span className={styles.error}>{errors.height.message}</span>
-            )}
-          </div>
-          <div>
-            <label className={styles.label} htmlFor="scale">
-              Scale
-            </label>
-            <input
-              className={styles.inputText}
+            <InputField
+              label="Scale"
               id="scale"
-              max={1}
-              min={0.1}
-              placeholder={DEFAULT_SCALE.toString()}
-              step={0.1}
               type="number"
-              {...register("scale")}
+              placeholder={DEFAULT_SCALE.toString()}
             />
-            {errors.scale && (
-              <span className={styles.error}>{errors.scale.message}</span>
-            )}
-          </div>
-          <div>
-            <label className={styles.label} htmlFor="quality">
-              Quality
-            </label>
-            <input
-              className={styles.inputText}
+            <InputField
+              label="Quality"
               id="quality"
-              max={100}
-              min={1}
-              step={1}
               type="number"
               placeholder={DEFAULT_QUALITY.toString()}
-              {...register("quality")}
             />
-            {errors.quality && (
-              <span className={styles.error}>{errors.quality.message}</span>
-            )}
+            <InputField label="Full Page" id="fullPage" type="checkbox" />
           </div>
-          <div>
-            <label className={styles.label} htmlFor="fullPage">
-              Full Page
-            </label>
-            <input
-              className={styles.checkbox}
-              id="fullPage"
-              type="checkbox"
-              {...register("fullPage")}
-            />
-            {errors.fullPage && (
-              <span className={styles.error}>{errors.fullPage.message}</span>
-            )}
+          <InputField label="API Key" id="key" type="text" />
+          <div className="mt-2">
+            <button className={styles.button} type="submit">
+              Generate Screenshot URL
+            </button>
           </div>
-        </div>
-        <div>
-          <label className={styles.label} htmlFor="url">
-            API Key
-          </label>
-          <input
-            className={styles.inputText}
-            type="text"
-            id="key"
-            placeholder="your-api-key"
-            {...register("key")}
-          />
-          {errors.key && (
-            <span className={styles.error}>{errors.key.message}</span>
-          )}
-        </div>
-        <div className="mt-2">
-          <button className={styles.button} type="submit">
-            Generate Screenshot URL
-          </button>
-        </div>
-      </form>
+        </form>
+      </FormProvider>
 
       <div>
         <h3>Generated URL</h3>
@@ -216,6 +135,39 @@ export const UrlBuilderForm = () => {
           </code>
         </div>
       </div>
+    </div>
+  );
+};
+
+const InputField = ({
+  label,
+  id,
+  ...props
+}: {
+  label: string;
+  id: keyof Inputs;
+} & React.InputHTMLAttributes<HTMLInputElement>) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext<Inputs>();
+
+  return (
+    <div>
+      <label className={styles.label} htmlFor={id}>
+        {label}
+      </label>
+      <input
+        id={id}
+        className={
+          props.type === "checkbox" ? styles.checkbox : styles.inputText
+        }
+        {...props}
+        {...register(id)}
+      />
+      {errors[id] && (
+        <span className={styles.error}>{errors[id]?.message}</span>
+      )}
     </div>
   );
 };
